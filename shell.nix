583d4e602d5eb504048e7ec4512760ae32a82872
deraffe <git@effa.red>
2025-05-01 18:22:33 +0200

    build: Add pre-commits

--- shell.nix
+++ shell.nix
@@ -1,4 +1,24 @@
 let
   sources = import ./npins;
   pkgs = import sources.nixpkgs { };
-in pkgs.mkShell { buildInputs = with pkgs; [ just python3 uv ]; }
+  pre-commit = (import sources."git-hooks.nix").run {
+    src = ./.;
+    default_stages = [
+      "pre-commit"
+      "pre-push"
+    ];
+    hooks = {
+      nixfmt-rfc-style.enable = true;
+      shellcheck.enable = true;
+    };
+  };
+  shellPackages = with pkgs; [
+    just
+    python3
+    uv
+  ];
+in
+pkgs.mkShell {
+  inherit (pre-commit) shellHook;
+  buildInputs = shellPackages ++ pre-commit.enabledPackages;
+}
